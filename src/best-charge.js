6cbf7a2550bce12c10fb3b25b595355305b10c83
WENGEM <emilio.wang@oocl.com>
2019-07-11 08:20:27 +0800

    fix the bug and uniform code style

--- src/best-charge.js
+++ src/best-charge.js
@@ -10,12 +10,14 @@ function getHalfPriceFood(){
 }
 
 // 27min
-function printReceipt(totalPrice, FoodInfos, halfPriceFood, firstPreference, secondPreference){
-  let receipt = `============= 订餐明细 =============\n`
-  FoodInfos.forEach((value) => {
-    let oneFoodPrice = value.price * value.count;
-    totalPrice += oneFoodPrice;
-    receipt += `${value.name} x ${value.count} = ${parseInt(oneFoodPrice)}元\n`
+function printReceipt(totalPrice, itemFoods, halfPriceFood, firstPreference, secondPreference){
+  let receipt = `============= 订餐明细 =============
+`
+  itemFoods.forEach((value) => {
+    let foodPrice = value.price * value.count;
+    totalPrice += foodPrice;
+    receipt += `${value.name} x ${value.count} = ${parseInt(foodPrice)}元
+`
 
     if(value.isHalfPriceFood){
       halfPriceFood.push(value.name);
@@ -25,37 +27,42 @@ function printReceipt(totalPrice, FoodInfos, halfPriceFood, firstPreference, sec
 
   if(totalPrice > 30){
     secondPreference = 6;
-    receipt += `-----------------------------------\n`;
-    receipt += `使用优惠:\n`;
+    receipt += `-----------------------------------
+使用优惠:
+`;
     if(firstPreference >= secondPreference) {
       totalPrice -= firstPreference;
-      receipt += `指定菜品半价(${halfPriceFood.join('，')})，省${firstPreference}元\n`;
+      receipt += `指定菜品半价(${halfPriceFood.join('，')})，省${firstPreference}元
+`;
     }else{
       totalPrice -= 6;
-      receipt += `满30减6元，省6元\n`;
+      receipt += `满30减6元，省6元
+`;
     }
   }else{
     if(firstPreference != 0){
       totalPrice -= firstPreference;
       receipt += `-----------------------------------
 使用优惠:
-指定菜品半价(${halfPriceFood.join('，')})，省${firstPreference}元\n`;
+指定菜品半价(${halfPriceFood.join('，')})，省${firstPreference}元
+`;
     }
   }
 
   receipt += `-----------------------------------
 总计：${parseInt(totalPrice)}元
-===================================\n`;
+===================================
+`;
   return receipt;
 }
 
 // 12min
 function bestCharge(selectedItems) {
   let totalPrice = 0;
-  let firstPreference = 0;
-  let secondPreference = 0;
   let halfPriceFood = [];
   let allHalfPriceFood = getHalfPriceFood();
+  let firstPreference = 0;
+  let secondPreference = 0;
   let itemFoods = [];
   getItemFoods(selectedItems, allHalfPriceFood, itemFoods);
   let receipt = printReceipt(totalPrice, itemFoods, halfPriceFood, firstPreference, secondPreference);
@@ -63,17 +70,17 @@ function bestCharge(selectedItems) {
 }
 
 // 25min
-function getItemFoods(selectedItems, allHalfPriceFood, FoodInfos){
+function getItemFoods(selectedItems, allHalfPriceFood, itemFoods){
   var allFood = loadAllItems();
   selectedItems.map((value) => {
     let items = value.split("x");
-    allFood.forEach(elem => {
-      if(elem.id == items[0].trim()){
-        FoodInfos.push({
-          name: elem.name,
-          price: elem.price,
-          count: items[1].trim(),
-          isHalfPriceFood: allHalfPriceFood[0].items.includes((items[0].trim()))
+    allFood.forEach(element => {
+      if(element.id == items[0].trim()){
+        itemFoods.push({
+          name : element.name,
+          price : element.price,
+          count : items[1].trim(),
+          isHalfPriceFood : allHalfPriceFood[0].items.includes((items[0].trim()))
         })
       }
     });
